c7b8e165ad04a6531a84214352a96ee3f4194c34
Karlos Álvarez <kalvarez@auvik.com>
2019-08-12 10:33:15 +0200

    Format minor changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return make_response(jsonify({'status':'ok'}), 200)
+  return make_response(jsonify({'status':'ok'}), 200)
 
 if __name__ == '__main__':
 	app.run(debug=True)
